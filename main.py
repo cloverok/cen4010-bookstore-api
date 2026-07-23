42fee05d8fd31ff56c90e2444e9b04ec7c6dc0d8
ElectrifyXD <gdelectrify@gmail.com>
2026-07-23 03:48:51 -0500

    Register browse blueprint in main

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+from profileManagement import profile_bp
+from wishlist import wishlist_bp
+from cart import cart_bp
+from bookDetails import book_bp
+from book_browsing import book_browsing_bp
+
+app = Flask(__name__)
+
+# Register feature endpoints in main file
+app.register_blueprint(profile_bp)
+app.register_blueprint(wishlist_bp)
+app.register_blueprint(cart_bp)
+app.register_blueprint(book_bp)
+app.register_blueprint(book_browsing_bp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
